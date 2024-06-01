5d282c6d3c7347bc3f8c8e7a6c86036ce7c35399
Leonid Andreev <andreevleonid12@gmail.com>
2024-06-01 23:20:07 +0300

    UserDaoJDBCImpl implemented

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,21 +10,6 @@ public class Util {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/" + DB_DBNAME + "?user=" + DB_USER + "&password=" + DB_PASSWORD + "&serverTimezone=UTC";
 
 
-    public static void main(String[] args) {
-
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            Statement stmt = connection.createStatement();
-            ResultSet resultSet = stmt.executeQuery("SELECT * FROM " + DB_TABLE);
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name"));
-            }
-        } catch (
-                SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
     public static Connection getConnection() {
         Connection connection = null;
         try {
